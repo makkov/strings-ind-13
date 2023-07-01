1f6860c62ef003ca0bb6980cb54da12131b89760
maks <maxim_86nva@mail.ru>
2023-07-01 13:05:50 +0300

    дз по строкам

--- src/com/hw/maxim/Main.java
+++ src/com/hw/maxim/Main.java
@@ -3,6 +3,41 @@ package com.hw.maxim;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        System.out.printf("ФИО сотрудника — %s", fullName);
+    }
+
+    public static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+        String fullNameInUpperCase = fullName.toUpperCase();
+        System.out.println(fullNameInUpperCase);
+    }
+
+    public static void task3() {
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+
+        String fullName = String.format("%s %s %s", lastName, firstName, middleName);
+
+        char incorrectSymbol = 'ё';
+        char correctSymbol = 'е';
+
+        String correctFullName = fullName.replace(incorrectSymbol, correctSymbol);
+        System.out.println(correctFullName);
     }
 }
